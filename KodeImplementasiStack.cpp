284cb2ef013b843f43e7bcf2f574d72e53b1792f
Faiz Nadhif <146604977+Antiether@users.noreply.github.com>
2024-05-29 21:33:42 +0700

    Update KodeImplementasiStack.cpp

--- KodeImplementasiStack.cpp
+++ KodeImplementasiStack.cpp
@@ -5,3 +5,16 @@
 #include <algorithm>
 #include <stack>
 using namespace std;
+
+int main() {
+    string input;
+    getline(cin, input);
+
+    vector<string> infix = KonversiInfix(input);
+    vector<string> postfix = KonversiPostfix(infix);
+
+    int result = evaluasi(postfix);
+    cout << result << endl;
+
+return 0;
+}
